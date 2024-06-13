2700eb21c109a92e2829bcc8e9b75af6503525c3
Hasana0777 <137383538+Hasana0777@users.noreply.github.com>
2024-06-13 19:59:36 +0530

    Add files via upload
    Simple Digital Clock using React js

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+ //import App from './App.jsx'
+//import Bmi from './Bmi.jsx';
+
+import Watch from './Watch.jsx';
+import './Watch.css';
+//import Pass from "./Pass.jsx";
+//import"./Qrcode.css";
+//import  {Qrcode} from "./Qrcode";
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+<Watch/>
+  </React.StrictMode>
+);
